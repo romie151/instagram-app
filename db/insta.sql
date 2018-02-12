a87d441300b975c88bcd922e75c6b1de639c2f51
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-02-12 16:55:06 -0500

    added new queries

--- db/insta.sql
+++ db/insta.sql
@@ -5,7 +5,7 @@ CREATE DATABASE insta;
 
 CREATE TABLE users (
   ID SERIAL PRIMARY KEY,
-  username VARCHAR,
+  username VARCHAR UNIQUE,
   password_digest VARCHAR, 
   email VARCHAR, 
   user_followers VARCHAR[], 
@@ -18,6 +18,10 @@ INSERT INTO users (username, password_digest, email, user_followers, user_follow
   VALUES ('Tyler21', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq', 
   'luiza@gmail.com', '{"john", "steven"}', '{"brian"}');
 
+  INSERT INTO users (username, password_digest, email, user_followers, user_following)
+  VALUES ('Anna', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcsdgfg', 
+  'anna@gmail.com', '{"steven"}', '{"brian"}');
+
 
 CREATE TABLE images (
   ID SERIAL PRIMARY KEY,
@@ -28,3 +32,6 @@ CREATE TABLE images (
 
 INSERT INTO images (img_URL, img_likes, user_ID)
   VALUES ('http:hsjghsdg.jpg', '{"john", "steven", "luiza"}', 1);
+
+INSERT INTO images (img_URL, img_likes, user_ID)
+  VALUES ('http:dsghjdsg.jpg', '{"john", "steven", "luiza"}', 2);
--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,141 @@
 const db = require("./index");
+
+const authHelpers = require("../auth/helpers");
+const passport = require("../auth/local");
+
+//gets all info for a single user 
+function getSingleUser(req, res, next) {
+  db
+    .any("select * from users join images on users.id = images.user_id where username = ${username}", req.user)
+    .then(function(data) {
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Fetched one user"
+      });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
+
+function getAllUserImages(req, res, next) {
+  db
+    .any("SELECT img_url FROM users JOIN images ON users.id = images.user_id")
+    .then(function(data) {
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Fetched all user images"
+      });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
+//gets all image urls for a single user
+function getSingleUserImages(req, res, next) {
+  db
+    .any("SELECT img_url FROM users JOIN images ON users.id = images.user_id WHERE username = ${username}", req.user)
+    .then(function(data) {
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Fetched one user's image"
+      });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
+
+function addImage(req, res, next) { 
+  db
+    .one("INSERT INTO images (img_url, img_likes, user_ID) VALUES (${img_url}, ${img_likes}, ${user_id}", req.user)
+    .then(function(data) {
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Added one image"
+      });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
+
+function addLike(req, res, next) { 
+  db
+    .one("UPDATE images SET img_likes = array_cat(img_likes, '${username}') WHERE user_id= ${user_id}", req.user)
+    .then(function(data) {
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Added one like"
+      });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
+
+function addFollower(req, res, next) { 
+  db
+    .one("UPDATE images SET img_likes = array_cat(img_likes, '${username}') WHERE user_id= ${user_id}", req.user)
+    .then(function(data) {
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Added one like"
+      });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+}
+
+function loginUser(req, res, next) {
+  passport.authenticate("local", {});
+}
+
+function logoutUser(req, res, next) {
+  req.logout();
+  res.status(200).send("log out success");
+}
+
+function createUser(req, res, next) {
+  if(req.body.password.length <= 6) { 
+    res.status(200).json({ 
+      message: `password must be longer than 6 characters`
+    })
+    return
+  }
+  const hash = authHelpers.createHash(req.body.password);
+  console.log("createuser hash: ", hash);
+  db
+    .none(
+      "INSERT INTO users (username, password_digest, email) VALUES (${username}, ${password}, ${email})",
+      { username: req.body.username, password: hash, email: req.body.email }
+    )
+    .then(() => {
+      res.status(200).json({
+        message: `created user: ${req.body.username}`
+      }); 
+      console.log('calling next ***')
+      next()
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send("error creating user");
+    });
+}
+
+module.exports = {
+getSingleUser: getSingleUser,
+getSingleUserImages: getSingleUserImages,  
+loginUser: loginUser, 
+logoutUser: logoutUser, 
+createUser: createUser, 
+addImage: addImage, 
+addLike: addLike
+}; 
