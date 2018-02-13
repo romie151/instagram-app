fe87c673cda1a35e64e98513c47dedd3ff0312c9
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-02-12 21:43:31 -0500

    add user description column to users table

--- db/insta.sql
+++ db/insta.sql
@@ -9,19 +9,20 @@ CREATE TABLE users (
   password_digest VARCHAR, 
   full_name VARCHAR,
   email VARCHAR, 
+  user_description VARCHAR, 
   user_followers VARCHAR[], 
   user_following VARCHAR[]
 );
 
 /* tyler, password: 123456 */
 
-INSERT INTO users (username, password_digest, full_name, email, user_followers, user_following)
+INSERT INTO users (username, password_digest, full_name, email, user_description, user_followers, user_following)
   VALUES ('Tyler21', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcooZWq', 'Tyler Wentworth',
-  'luiza@gmail.com', '{"john", "steven"}', '{"brian"}');
+  'tyler@gmail.com', 'Animal lover who hikes all the time','{"john", "steven"}', '{"brian"}');
 
-  INSERT INTO users (username, password_digest, full_name, email, user_followers, user_following)
+  INSERT INTO users (username, password_digest, full_name, email, user_description, user_followers, user_following)
   VALUES ('Anna', '$2a$10$brAZfSmByFeZmPZ/MH5zne9YDhugjW9CtsBGgXqGfix0g1tcsdgfg', 'Anna Thornton',
-  'anna@gmail.com', '{"steven"}', '{"brian"}');
+  'anna@gmail.com', 'Coffee drinker and starter upper','{"steven"}', '{"brian"}');
 
 
 CREATE TABLE images (
